a59e9823e31fcf063499492b43f43ea7ff3ae8df
Ronald Sumbayak <kandaz611@yahoo.com>
2018-09-12 04:29:27 +0700

    Update ImageDataRepository

--- data/src/main/kotlin/com/rsmbyk/course/mdp/data/repository/ImageDataRepository.kt
+++ data/src/main/kotlin/com/rsmbyk/course/mdp/data/repository/ImageDataRepository.kt
@@ -1,17 +1,34 @@
 package com.rsmbyk.course.mdp.data.repository
 
-import android.os.Environment
+import android.content.Context
+import com.rsmbyk.course.mdp.data.api.volley.VolleyRequestQueue
+import com.rsmbyk.course.mdp.data.api.volley.request.UploadSingleImageRequest
+import com.rsmbyk.course.mdp.domain.common.VolleyRequestCallback
 import com.rsmbyk.course.mdp.domain.repository.ImageRepository
 import java.io.File
 
-class ImageDataRepository: ImageRepository {
+private fun String.endsWithIgnoreCase (suffix: String) =
+    endsWith (".$suffix", true)
 
-    override fun getImages (): List<File> =
-        File (Environment.getExternalStoragePublicDirectory (Environment.DIRECTORY_PICTURES), "MDP").let {
-            if (!it.exists ())
-                it.mkdirs ()
-            it
-                .listFiles ()
-                .sortedByDescending (File::lastModified)
-        }
+class ImageDataRepository (private val context: Context, private val volleyRequestQueue: VolleyRequestQueue)
+    : ImageRepository {
+
+    private val imageFileExtensions = arrayOf ("jpg", "jpeg", "png", "gif")
+
+    override fun getImages (directory: File): List<File> =
+        directory
+            .listFiles (::isImageFile)
+            .sortedByDescending (File::lastModified)
+
+    private fun isImageFile (dir: File, name: String): Boolean =
+        imageFileExtensions.any (name::endsWithIgnoreCase)
+
+    override fun uploadImage (
+            url: String,
+            nrp: String,
+            imageFile: File,
+            callback: VolleyRequestCallback<String>) {
+        val request = UploadSingleImageRequest (context, url, nrp, imageFile, callback)
+        volleyRequestQueue.add (request)
+    }
 }
