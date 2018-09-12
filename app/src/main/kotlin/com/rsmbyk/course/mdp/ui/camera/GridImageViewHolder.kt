275b2154d273045a396488e8613f588f4ccdaf2d
Ronald Sumbayak <kandaz611@yahoo.com>
2018-09-12 10:25:46 +0700

    Update GridImageViewHolder

---
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/ui/camera/GridImageViewHolder.kt
@@ -0,0 +1,22 @@
+package com.rsmbyk.course.mdp.ui.camera
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.rsmbyk.course.mdp.R
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.viewholder_image_grid.*
+import java.io.File
+
+class GridImageViewHolder (override val containerView: View)
+    : RecyclerView.ViewHolder (containerView), LayoutContainer {
+
+    fun bind (file: File) {
+        Glide
+            .with (itemView)
+            .load (file)
+            .apply (RequestOptions ().placeholder (R.drawable.ic_image_placeholder))
+            .into (image)
+    }
+}
