54375102a39bd92c6148779361e450cf9acb5ba7
Ronald Sumbayak <kandaz611@yahoo.com>
2018-09-12 15:54:37 +0700

    Simplify outRect assignment

--- app/src/main/kotlin/com/rsmbyk/course/mdp/common/SpaceItemDecoration.kt
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/common/SpaceItemDecoration.kt
@@ -22,40 +22,18 @@ class SpaceItemDecoration (
 
         val position = parent.getChildLayoutPosition (view)
 
-        if (position < spanCount) {
-            if (edgeSpace)
-                outRect.top = space
-        } else {
-            outRect.top = space / 2
-        }
+        outRect.top = setOutRect (position < spanCount)
 
         if (spanCount > 1) {
-            if (position % spanCount == 0) {
-                if (edgeSpace)
-                    outRect.left = space
-            } else {
-                outRect.left = space / 2
-            }
-
-            if (position % spanCount == spanCount - 1) {
-                if (edgeSpace)
-                    outRect.right = space
-            } else {
-                outRect.right = space / 2
-            }
+            outRect.left = setOutRect (position % spanCount == 0)
+            outRect.right = setOutRect (position % spanCount == spanCount - 1)
         }
 
         // distance to the edge of spanCount.
         // if spanCount is 4 and the position
         // of current item is 6, the odd will be 2 (8 - 6).
         val odd = parent.adapter!!.itemCount % spanCount
-
-        if (position >= parent.adapter!!.itemCount - odd) {
-            if (edgeSpace)
-                outRect.bottom = space
-        } else {
-            outRect.bottom = space / 2
-        }
+        outRect.bottom = setOutRect (position >= parent.adapter!!.itemCount - odd)
     }
 
     private fun setOutRect (edgeCondition: Boolean): Int =
