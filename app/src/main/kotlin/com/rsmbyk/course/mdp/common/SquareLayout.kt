26956e1360aa2c82369991b16d4c3b4710a3f72b
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-11 01:03:19 +0700

    Add SquareLayout

---
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/common/SquareLayout.kt
@@ -0,0 +1,43 @@
+package com.rsmbyk.course.mdp.common
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+import com.rsmbyk.course.mdp.R
+
+class SquareLayout (context: Context, attrs: AttributeSet): RelativeLayout (context, attrs) {
+
+    private val pivot: Pivot
+
+    init {
+        val styledAttributes: TypedArray = context.obtainStyledAttributes (attrs, R.styleable.SquareLayout, 0, 0)
+        val pivotValue: Int = styledAttributes.getInteger (R.styleable.SquareImageView_pivot, -1)
+        assert (pivotValue != -1) { "Pivot must be set" }
+        pivot = Pivot.values ()[pivotValue]
+        styledAttributes.recycle ()
+        post {
+            getMeasuredPivot ().let {
+                layoutParams.width = it
+                layoutParams.height = it
+                requestLayout ()
+            }
+        }
+    }
+
+    private fun getMeasuredPivot (): Int {
+        return when (pivot) {
+            Pivot.WIDTH -> measuredWidth
+            Pivot.HEIGHT -> measuredHeight
+            Pivot.SMALLEST -> minOf (measuredWidth, measuredHeight)
+            Pivot.LARGEST -> maxOf (measuredWidth, measuredHeight)
+        }
+    }
+
+    enum class Pivot {
+        WIDTH,
+        HEIGHT,
+        SMALLEST,
+        LARGEST
+    }
+}
