ec41284d550bd2ab204687d1bb3f6e3fc2ff3980
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-11 11:02:23 +0700

    Fix PermissionUtil callback not get called in fragments

--- app/src/main/kotlin/com/rsmbyk/course/mdp/common/PermissionUtil.kt
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/common/PermissionUtil.kt
@@ -3,20 +3,22 @@ package com.rsmbyk.course.mdp.common
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import java.util.*
 
-class PermissionUtil (private val activity: Activity) {
+class PermissionUtil (private val activity: Activity? = null, private val fragment: Fragment? = null) {
 
+    private val context = activity ?: fragment?.context
     private val requestQueue = LinkedList<Request> ()
 
     fun requestPermission (
             permissions: Array<String>,
             onGranted: (requestCode: Int) -> Unit,
-            onRejected: ((requestCode: Int) -> Unit)? = null,
+            onDenied: ((requestCode: Int) -> Unit)? = null,
             requestCode: Int? = null): Int {
-        val finalRequestCode = requestCode ?: Random ().nextInt ()
-        requestQueue.push (Request (activity, permissions, finalRequestCode, onGranted, onRejected))
+        val finalRequestCode = requestCode ?: Random ().nextInt (65535)
+        requestQueue.push (Request (permissions, finalRequestCode, onGranted, onDenied))
         processNextRequest ()
         return finalRequestCode
     }
@@ -24,21 +26,39 @@ class PermissionUtil (private val activity: Activity) {
     fun requestPermission (
             permission: String,
             onGranted: (requestCode: Int) -> Unit,
-            onRejected: ((requestCode: Int) -> Unit)? = null,
+            onDenied: ((requestCode: Int) -> Unit)? = null,
             requestCode: Int? = null): Int =
-        requestPermission (arrayOf (permission), onGranted, onRejected, requestCode)
+        requestPermission (arrayOf (permission), onGranted, onDenied, requestCode)
+
+    fun requestPermission (
+            permission: Array<String>,
+            onGranted: () -> Unit,
+            onDenied: (() -> Unit)? = null,
+            requestCode: Int? = null): Int =
+        requestPermission (permission, { _ -> onGranted () }, { _ -> onDenied?.invoke () }, requestCode)
+
+    fun requestPermission (
+            permission: String,
+            onGranted: () -> Unit,
+            onDenied: (() -> Unit)? = null,
+            requestCode: Int? = null): Int =
+        requestPermission (arrayOf (permission), { _ -> onGranted () }, { _ -> onDenied?.invoke () }, requestCode)
 
     private fun processNextRequest () {
         requestQueue.firstOrNull ()?.apply {
             if (permissions.all (::isGranted))
                 onGranted (requestCode)
-            else
-                ActivityCompat.requestPermissions (activity, permissions, requestCode)
+            else {
+                if (activity != null)
+                    ActivityCompat.requestPermissions (activity, permissions, requestCode)
+                else if (fragment != null)
+                    fragment.requestPermissions (permissions, requestCode)
+            }
         }
     }
 
-    private fun isGranted (permission: String) =
-        ContextCompat.checkSelfPermission (activity, permission) == PackageManager.PERMISSION_GRANTED
+    private fun isGranted (permission: String): Boolean =
+        ContextCompat.checkSelfPermission (context!!, permission) == PackageManager.PERMISSION_GRANTED
 
     fun handlePermissionResult (requestCode: Int, grantResults: IntArray) {
         requestQueue.firstOrNull ()?.apply {
@@ -46,7 +66,7 @@ class PermissionUtil (private val activity: Activity) {
                 if (grantResults.all (PackageManager.PERMISSION_GRANTED::equals))
                     onGranted (requestCode)
                 else
-                    onRejected?.invoke (requestCode)
+                    onDenied?.invoke (requestCode)
             }
         }
         requestQueue.removeFirst ()
@@ -54,10 +74,9 @@ class PermissionUtil (private val activity: Activity) {
     }
 
     class Request (
-        val activity: Activity,
         val permissions: Array<String>,
         val requestCode: Int,
         val onGranted: (requestCode: Int) -> Unit,
-        val onRejected: ((requestCode: Int) -> Unit)?
+        val onDenied: ((requestCode: Int) -> Unit)?
     )
 }
