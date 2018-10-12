b3bf7c1e8c78a728cc8b8238fb709bda6a8826c2
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-12 12:05:50 +0700

    Fix Camera Activity crash when returning result
    It seems like the byte array of the captured image too long to fit in
    the intent result. It will save the image in a temporary file and return
    the file path instead.

--- app/src/main/kotlin/com/rsmbyk/course/mdp/ui/camera/CameraActivity.kt
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/ui/camera/CameraActivity.kt
@@ -1,17 +1,37 @@
 package com.rsmbyk.course.mdp.ui.camera
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.otaliastudios.cameraview.CameraListener
 import com.rsmbyk.course.mdp.R
+import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.File
 
 class CameraActivity: AppCompatActivity () {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    companion object {
+        const val PICTURE_PATH_EXTRA = "com.rsmbyk.course.mdp.ui.camera.CameraActivity.PICTURE_EXTRA"
+    }
+
+    private val cameraListener = object : CameraListener () {
+        override fun onPictureTaken (jpeg: ByteArray?) {
+            val tempFile = File.createTempFile ("MDP-", null)
+            tempFile.deleteOnExit ()
+            tempFile.writeBytes (jpeg!!)
+
+            val intent = Intent ().putExtra (PICTURE_PATH_EXTRA, tempFile.canonicalPath)
+            setResult (Activity.RESULT_OK, intent)
+            finish ()
+        }
+    }
+
+    override fun onCreate (savedInstanceState: Bundle?) {
+        super.onCreate (savedInstanceState)
         setContentView (R.layout.activity_camera)
-        supportFragmentManager
-            .beginTransaction ()
-            .replace (R.id.fragment, CameraFragment (), CameraFragment::javaClass.name)
-            .commit ()
+        camera.setLifecycleOwner (this)
+        camera.addCameraListener (cameraListener)
+        btn_capture.setOnClickListener { camera.capturePicture () }
     }
 }
