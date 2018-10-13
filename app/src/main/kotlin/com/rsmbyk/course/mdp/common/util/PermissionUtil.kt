d74f8975b516b0f10adbc0aa7319da4e03db595d
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-14 01:43:16 +0700

    Update PermissionUtil.kt

--- app/src/main/kotlin/com/rsmbyk/course/mdp/common/util/PermissionUtil.kt
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/common/util/PermissionUtil.kt
@@ -18,7 +18,7 @@ class PermissionUtil (private val activity: Activity? = null, private val fragme
             onDenied: ((requestCode: Int) -> Unit)? = null,
             requestCode: Int? = null): Int {
         val finalRequestCode = requestCode ?: Random ().nextInt (65535)
-        requestQueue.push (Request(permissions, finalRequestCode, onGranted, onDenied))
+        requestQueue.push (Request (permissions, finalRequestCode, onGranted, onDenied))
         processNextRequest ()
         return finalRequestCode
     }
