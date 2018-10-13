12cdfafa50b5f12782e1eecb4ccd6b9b75d3ba1e
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-14 01:41:44 +0700

    Update SquareLayout.kt

--- app/src/main/kotlin/com/rsmbyk/course/mdp/common/view/SquareLayout.kt
+++ app/src/main/kotlin/com/rsmbyk/course/mdp/common/view/SquareLayout.kt
@@ -5,7 +5,6 @@ import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.widget.RelativeLayout
 import com.rsmbyk.course.mdp.R
-import com.rsmbyk.course.mdp.common.view.SquareLayout.Pivot
 
 class SquareLayout (context: Context, attrs: AttributeSet): RelativeLayout (context, attrs) {
 
