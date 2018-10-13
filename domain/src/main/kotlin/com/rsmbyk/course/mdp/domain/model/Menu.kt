12e5b83fa0a76ff006c3d6f054dd07781dc272f5
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-13 11:10:16 +0700

    Remove Menu

--- domain/src/main/kotlin/com/rsmbyk/course/mdp/domain/model/Menu.kt
+++
@@ -1,7 +0,0 @@
-package com.rsmbyk.course.mdp.domain.model
-
-enum class Menu {
-    CALCULATOR,
-    CAMERA,
-    NETWORKING
-}
--- domain/src/main/kotlin/com/rsmbyk/course/mdp/domain/repository/MenuRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.rsmbyk.course.mdp.domain.repository
-
-import io.reactivex.Observable
-
-interface MenuRepository {
-
-    fun menus (): Observable<List<String>>
-}
